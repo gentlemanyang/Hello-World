93e7805f911a8021d668b88a2a9328dd87a0afc4
yangjiajia <645035259@qq.com>
2017-07-28 22:13:33 +0800

    test

---
+++ WebApplication1/WebApplication1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace HelloWorld
+{
+    using System;
+    namespace HelloWorldApplication
+    {
+        class HelloWorld
+        {
+            static void Main(string[] args)
+            {
+                /* 我的第一个 C# 程序*/
+                Console.WriteLine("Hello World");
+                Console.ReadKey();
+            }
+        }
+    }
+}
